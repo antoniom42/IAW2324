46a49af7663d9a3bb703c34b106828f1cb7cd7c6
Antonio Manuel Eslava <antonioeslava04@iesamachado.org>
2023-11-06 14:24:49 +0100

    floren

---
+++ Repaso JS/ejer1.js
@@ -0,0 +1,26 @@
+//rojo
+function ocultarr() {
+    if (document.getElementById("rojo").style.display == "block") {
+        document.getElementById("rojo").style.display = "none";}
+        else {
+            document.getElementById("rojo").style.display = "block";
+        }
+}
+
+//verde
+function ocultarv() {
+    if (document.getElementById("verde").style.display == "block") {
+        document.getElementById("verde").style.display = "none";}
+        else {
+            document.getElementById("verde").style.display = "block";
+        }
+}
+
+//azul
+function ocultara() {
+    if (document.getElementById("azul").style.display == "block") {
+        document.getElementById("azul").style.display = "none";}
+        else {
+            document.getElementById("azul").style.display = "block";
+    }
+}
